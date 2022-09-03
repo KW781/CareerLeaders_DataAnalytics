b12e568f19fcf7e3902c2f64d0d324ed42f78861
KW781 <59013794+KW781@users.noreply.github.com>
2022-09-03 22:51:48 +1200

    added application progression plot to recruitment

---
+++ ApplicationProgressionPlot.m
@@ -0,0 +1,111 @@
+function [] = ApplicationProgressionPlot(file_name, top_headings, bottom_headings)
+    table_raw = readtable(file_name);
+    table = table2cell(table_raw);
+    
+    %find column numbers with employer data
+    start_column_number = -1;
+    end_column_number = -1;
+    for i = 1 : length(top_headings)
+        if WithinWord('Please indicate who you applied to and how far you got in the application process', top_headings{i})
+            start_column_number = i;
+        end
+        %if start column has been found, end the search when we've found a
+        %the last column of the company application question
+        if start_column_number ~= -1 && WithinWord('If you applied to other organisations', bottom_headings{i})
+            end_column_number = i - 1;
+            break;
+        end
+    end
+    
+    %extract table dimensions (number of students)
+    dimensions = size(table);
+    num_students = dimensions(1);
+    
+    %set up progression strings and counters for counting through spreadsheet
+    app_stages = {'Submitted application', 'Completed psychometric test', 'Completed a video interview',...
+                  'Attended assessment centre', 'Attended interview', 'Offered role', 'Accepted role'};
+    app_stage_counters = zeros(1, length(app_stages));
+    
+    %count up the application stages for all the students
+    for row = 1 : num_students
+        prev_employer_name = '';
+        submit_app = 0; %flag for whether student submitted application to current company
+        offered_role = 0; %flag for whether student was offered role for current company
+        for col = start_column_number : end_column_number
+            %if length of current table element is greater than 1, then
+            %that means the student selected that application stage
+            if length(table{row, col}) > 1
+                %extract employer name and current application stage from bottom heading
+                head_strings = split(bottom_headings{col}, ' - ');
+                employer_name = head_strings{1};
+                curr_app_stage = head_strings{2};
+                %if the current employer is different from the previous
+                %employer, reset the flags to indicate that the student hasn't
+                %submitted an application for current employer and hasn't been
+                %offered a role
+                if ~strcmp(employer_name, prev_employer_name)
+                    submit_app = 0;
+                    offered_role = 0;
+                end
+                for i = 1 : length(app_stages)
+                    if strcmp(app_stages{i}, curr_app_stage)
+                        app_stage_counters(i) = app_stage_counters(i) + 1;
+                        %if stage is 'submitted application', then set flag as appropriate
+                        if i == 1
+                            submit_app = 1;
+                        end
+                        %if the student got to any particular stage in the
+                        %application process, then they must've submitted an
+                        %application hence automatically increment that counter
+                        %if the student didn't select 'submitted application'
+                        %as well
+                        if i > 1 && ~submit_app
+                            app_stage_counters(1) = app_stage_counters(1) + 1;
+                        end
+                        %if stage is 'offered role', then set flag as appropriate
+                        if i == length(app_stages) - 1
+                            offered_role = 1;
+                        end
+                        %if the student accepted a role, then they must've also
+                        %been offered a role hence automatically increment that
+                        %counter if the student didn't select 'offered role'
+                        if i == length(app_stages) && ~offered_role
+                           app_stage_counters(length(app_stages) - 1) = app_stage_counters(length(app_stages) - 1) + 1;
+                        end
+                        break;
+                    end
+                end
+                prev_employer_name = employer_name;
+            end
+        end
+    end
+    
+    %ensure that only application stages with count greater than zero are plotted
+    options_index = 0;
+    final_app_stages = {};
+    final_app_stage_counters = [];
+    for i = 1 : length(app_stage_counters)
+        if app_stage_counters(i) > 0
+            options_index = options_index + 1;
+            final_app_stages{options_index} = app_stages{i};
+            final_app_stage_counters(options_index) = app_stage_counters(i);
+        end
+    end
+    
+    ordinal_final_app_stages = categorical(final_app_stages); %convert strings to categorical type
+    
+    %reorder the ordinal application stages since categorical() alphabetises them by default
+    ordinal_final_app_stages = reordercats(ordinal_final_app_stages, final_app_stages);
+    
+    %plot the data
+    colours = rand(length(ordinal_final_app_stages), 3);
+    bar_plot = bar(ordinal_final_app_stages, final_app_stage_counters, 'facecolor', 'flat');
+    bar_plot.CData = colours; %colour in the bars for the bar plot
+    text(1 : length(final_app_stage_counters),...
+        final_app_stage_counters,...
+        num2str(final_app_stage_counters'),...
+        'vert', 'bottom', 'horiz', 'center'); 
+    title('Number of students progressing to each application stage throughout recruitment');
+    xlabel('Application stage');
+    ylabel('Number of students')
+end
--- DataAnalysis.m
+++ DataAnalysis.m
@@ -18,31 +18,33 @@ disp('13. Plot of majors students are studying');
 disp('14. Plot of salaries offered to students for their roles')
 disp('15. Plot of genders of survey respondents')
 disp('16. Plot of NZ residency status of students')
+disp('17. Plot of application progression of students')
+disp('18. Plot of top/most popular employers')
 disp(newline);
 disp('P2B Survey Plots');
-disp('17. Plot of confidence of students in career areas before and after');
-disp('18. Plot of understanding after programme completion');
-disp('19. Plot of perceived value of group sessions');
-disp('20. Plot of perceived value of workshops');
-disp('21. Plot of perceived value of individual work');
-disp('22. Plot of how satisfied students were in using Canvas');
-disp('23. Plots of attendance at P2B events and workload');
+disp('19. Plot of confidence of students in career areas before and after');
+disp('20. Plot of understanding after programme completion');
+disp('21. Plot of perceived value of group sessions');
+disp('22. Plot of perceived value of workshops');
+disp('23. Plot of perceived value of individual work');
+disp('24. Plot of how satisfied students were in using Canvas');
+disp('25. Plots of attendance at P2B events and workload');
 disp(newline);
 disp('LinkedIn Destinations Plots');
-disp('24. Plot of most popular majors of graduates');
-disp('25. Plots of jobs taken by graduates in current period');
-disp('26. Plots of jobs taken by graduates in previous period');
-disp('27. Plots of jobs taken by graduates in 2nd furthest period');
-disp('28. Plots of jobs taken by graduates in furthest period');
-disp('29. Plot of companies worked at by graduates in current period');
-disp('30. Plot of companies worked at by graduates in previous period');
-disp('31. Plot of companies worked at by graduates in 2nd furthest period');
-disp('32. Plot of companies worked at by graduates in furthest period');
+disp('26. Plot of most popular majors of graduates');
+disp('27. Plots of jobs taken by graduates in current period');
+disp('28. Plots of jobs taken by graduates in previous period');
+disp('29. Plots of jobs taken by graduates in 2nd furthest period');
+disp('30. Plots of jobs taken by graduates in furthest period');
+disp('31. Plot of companies worked at by graduates in current period');
+disp('32. Plot of companies worked at by graduates in previous period');
+disp('33. Plot of companies worked at by graduates in 2nd furthest period');
+disp('34. Plot of companies worked at by graduates in furthest period');
 disp(newline);
 
 %request the plot number from the user, until a valid one is entered
 plot_number = -1;
-while ~(plot_number >= 1 && plot_number <= 32)
+while ~(plot_number >= 1 && plot_number <= 34)
     plot_number = input('Enter which number plot you want: ');
 end
 file_name = input('Enter the file name of the spreadsheet you want to analyse: ', 's');
@@ -61,7 +63,20 @@ if plot_number >= 7 && plot_number <= 16
     for i = 1 : num_cols
         headings{i} = raw_data{1, i};
     end
+elseif plot_number >= 17 && plot_number <= 18
+    %if recruitment plot where employer data is needed, extract the bottom
+    %headings as well because they contain the employer headings
+    [~, ~, raw_data] = xlsread(file_name);
+    dimensions = size(raw_data);
+    num_cols = dimensions(2);
+    top_headings = {};
+    bottom_headings = {};
+    for i = 1 : num_cols
+        top_headings{i} = raw_data{1, i};
+        bottom_headings{i} = raw_data{2, i};
+    end
 end
+    
 
 try
     switch plot_number
@@ -98,41 +113,45 @@ try
         case 16
             NZResidencyPlot(file_name, headings);
         case 17
-            ConfidencePlot(file_name);
+            ApplicationProgressionPlot(file_name, top_headings, bottom_headings);
         case 18
-            UnderstandingProgrammePlot(file_name);
+            TopEmployersPlot(file_name, top_headings, bottom_headings);
         case 19
-            GroupSessionPlot(file_name);
+            ConfidencePlot(file_name);
         case 20
-            WorkshopPlot(file_name);
+            UnderstandingProgrammePlot(file_name);
         case 21
-            IndividualWorkPlot(file_name);
+            GroupSessionPlot(file_name);
         case 22
-            CanvasSatisfactionPlot(file_name);
+            WorkshopPlot(file_name);
         case 23
-            WorkloadAttendancePlot(file_name);
+            IndividualWorkPlot(file_name);
         case 24
-            DestinationsMajorsPlot(file_name);
+            CanvasSatisfactionPlot(file_name);
         case 25
-            JobTitlesPlot(file_name, 4);
+            WorkloadAttendancePlot(file_name);
         case 26
-            JobTitlesPlot(file_name, 1);
+            DestinationsMajorsPlot(file_name);
         case 27
-            JobTitlesPlot(file_name, 2);
+            JobTitlesPlot(file_name, 4);
         case 28
-            JobTitlesPlot(file_name, 3);
+            JobTitlesPlot(file_name, 1);
         case 29
-            CompaniesPlot(file_name, 4);
+            JobTitlesPlot(file_name, 2);
         case 30
-            CompaniesPlot(file_name, 1);
+            JobTitlesPlot(file_name, 3);
         case 31
-            CompaniesPlot(file_name, 2);
+            CompaniesPlot(file_name, 4);
         case 32
+            CompaniesPlot(file_name, 1);
+        case 33
+            CompaniesPlot(file_name, 2);
+        case 34
             CompaniesPlot(file_name, 3);
     end
 catch
     disp('Something went wrong. Try again. Make sure you have given the correct spreadsheet for the plot you want.')
-    if plot_number >= 23 && plot_number <= 32
+    if plot_number >= 26 && plot_number <= 34
         disp('You are generating a plot for the LinkedIn Destinations project. Make sure you have entered the data into the spreadsheet correctly.');
     end
 end
