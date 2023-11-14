d857569c1e688a33bbb3691aec696afac7aceaeb
KW781 <aarnobguha@gmail.com>
2023-11-15 02:24:33 +1300

    fix: :bug: fixed various crash issues and percent labels

--- JobMotivationPlot.m
+++ JobMotivationPlot.m
@@ -16,28 +16,29 @@ function [] = JobMotivationPlot(file_name, headings)
     num_students = dimensions(1);
     
     %THIS ARRAY NEEDS TO BE SET UP MANUALLY AFTER REVIEWING THE SPREADSHEET BY A HUMAN
-    motivations = {, 'Other'};
+    motivations = {'Salary', 'Culture', 'Learning', 'Development', 'Benefits', 'Other'};
     %this array is for the storing of synonyms of the above motivations
     %e.g. motivations_synonyms{2} is a cell array of synonyms for
     %motivations{2}
-    motivations_synonyms = {{}, {}};
+    motivations_synonyms = {{'Salary', 'Pay', 'Money', 'Wages', 'Wage', 'Renumeration'}, {'Culture', 'Environment'}, {'Learning', 'Education'},...
+        {'Development', 'Growth', 'Progression', 'Opportunities'}, {'Benefits', 'Perks', 'Privileges', 'Incentives'}};
     motivation_counters = zeros(1, length(motivations));
     
     %for each student find what motivates them to say yes against the list of motivations specified
     for i = 1 : num_students
         found = 0;
-        for j = 1 : length(motivations)
+        for j = 1 : length(motivations) - 1 %-1 to account for 'other'
             %go through the list of synonyms to account for synonyms of the motivations being used 
             for k = 1 : length(motivations_synonyms{j})
-                if WithinWord(motivation_synonyms{j}{k}, table{i, column_number})
+                if WithinWord(motivations_synonyms{j}{k}, table{i, column_number})
                     motivation_counters(j) = motivation_counters(j) + 1;
                     found = 1;
                     break;
                 end
             end
         end
-        %if no match was found against any of the motivations, increment the 'Other' count
-        if ~found
+        %if no match was found against any of the motivations, and the response isn't empty, increment the 'Other' count
+        if ~found && length(table{i, column_number}) > 1
             motivation_counters(length(motivation_counters)) = motivation_counters(length(motivation_counters)) + 1;
         end
     end
@@ -47,10 +48,10 @@ function [] = JobMotivationPlot(file_name, headings)
     final_motivations = {};
     motivation_proportions = [];
     for i = 1 : length(motivation_counters)
-        if round((motivation_counters(i) / sum(motivation_counters)) * 100, 2) ~= 0
+        if round((motivation_counters(i) / num_students) * 100, 2) ~= 0
             options_index = options_index + 1;
             final_motivations{options_index} = motivations{i};
-            motivation_proportions(options_index) = round((motivation_counters(i) / sum(motivation_counters)) * 100, 2);
+            motivation_proportions(options_index) = round((motivation_counters(i) / num_students) * 100, 2);
         end
     end
     
@@ -68,9 +69,9 @@ function [] = JobMotivationPlot(file_name, headings)
     xlim([0, min([100, max([limits(2), max(motivation_proportions) + 5, max(motivation_proportions) * 1.1])])]);
     xtips1 = bar_plot(1).YEndPoints + 0.3;
     ytips1 = bar_plot(1).XEndPoints;
-    labels1 = string(bar_plot(1).YData);
+    labels1 = string(bar_plot(1).YData) + '%';
     text(xtips1,ytips1,labels1,'VerticalAlignment','middle');  %add text labels for the percentage to each bar
     title('Motivations for students to say yes to a job opportunity');
     xlabel('Motivations');
-    yabel('Percentage of students');
+    ylabel('Percentage of students');
 end
